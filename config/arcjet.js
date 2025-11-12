f2ecddb36f9283f0740f7b5777adf37f1904df2e
Keshav Nandwal <001keshavnandwal@gmail.com>
2025-11-12 15:14:22 +0530

    change mode from dry-run to live

--- config/arcjet.js
+++ config/arcjet.js
@@ -4,13 +4,13 @@ const aj = arcjet({
     key: process.env.ARCJET_KEY,
     characteristics: ["ip.src"],
     rules: [
-        shield({ mode: "DRY_RUN" }),
+        shield({ mode: "LIVE" }),
         detectBot({
-            mode: "DRY_RUN",
+            mode: "LIVE",
             allow: ["CATEGORY:SEARCH_ENGINE"],
         }),
         tokenBucket({
-            mode: "DRY_RUN",
+            mode: "LIVE",
             refillRate: 5, // Refill 5 tokens per interval
             interval: 10, // Refill every 10 seconds
             capacity: 10, // Bucket capacity of 10 tokens
