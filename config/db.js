f1ff7d8459356a9c594866c4b2fc185f774a0b6c
Keshav Nandwal <001keshavnandwal@gmail.com>
2025-11-12 15:28:10 +0530

    bd bug fix

--- config/db.js
+++ config/db.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(process.env.MONGODB_URI, {
             serverSelectionTimeoutMS: 10000 // timeout after 10s
         });
-        console.log("✅ MongoDB connected", conn);
+        console.log("✅ MongoDB connected");
     } catch (err) {
         console.error("❌ DB Connection Error:", err.message);
         process.exit(1);
